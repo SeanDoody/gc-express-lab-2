09a106ab76acaa554bb0e9769fcd038f92872856
sean <sdoody95@gmail.com>
2022-03-14 13:45:30 -0400

    connected to angular lab #4

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cartItems = require('./routes/cart-items.js');
 app.use(express.json());
 app.use(cors());
 app.use('/cart-items', cartItems);
+app.use(express.static(__dirname + '/public'));
 
 const port = 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
